a63e89ca396338e8ab997a3b82b0d3664cbd0c81
vinicius-16 <vinicius16@github.com>
2020-06-09 10:07:00 -0300

    findbyid

--- server.js
+++ server.js
@@ -13,16 +13,67 @@ app.listen(port, ()=>{
 	console.log(`server is listening on port:${port}`)
 })
 
+app.post('/users',(req,res)=>{
+  User.create(
+    {
+      name:req.body.newData.name,
+      email:req.body.newData.email,
+      password:req.body.newData.password
+    },
+    (err,data)=>{
+    if (err){
+      res.json({success: false,message: err})
+    } else if (!data){
+      res.json({success: false,message: "Not Found"})
+    } else {
+      res.json({success: true,data: data})
+    }
+  })
+})
+
 // CREATE
 app.post('/users',(req,res)=>{
-  // User.create()
+  //User.create()
+  User.create(
+    {
+      name:req.body.newData.name,
+      email:req.body.newData.email,
+      password:req.body.newData.password
+    },
+    (err,data)=>{
+    if (err){
+      res.json({success: false,message: err})
+    } else if (!data){
+      res.json({success: false,message: "Not Found"})
+    } else {
+      res.json({success: true,data: data})
+    }
+  })
 })
 
 app.route('/users/:id')
-// READ
+
 .get((req,res)=>{
-  // User.findById()
+  User.findById(req.params.id,(err,data)=>{
+    if (err){
+      res.json({
+        success: false,
+        message: err
+      })
+    } else if (!data){
+      res.json({
+        success: false,
+        message: "Not Found"
+      })
+    } else {
+      res.json({
+        success: true,
+        data: data
+      })
+    }
+  })
 })
+
 // UPDATE
 .put((req,res)=>{
   // User.findByIdAndUpdate()
@@ -31,3 +82,4 @@ app.route('/users/:id')
 .delete((req,res)=>{
   // User.findByIdAndDelete()
 })
+
